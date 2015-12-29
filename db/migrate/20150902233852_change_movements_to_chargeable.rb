8da6cfa89e9f6db4c22eac784ec0b7ed23cb137e
Dmitry Rocha <dmitryrck@gmail.com>
2015-12-29 13:30:55 -0300

    Update migration to work with postgresql

--- db/migrate/20150902233852_change_movements_to_chargeable.rb
+++ db/migrate/20150902233852_change_movements_to_chargeable.rb
@@ -2,7 +2,7 @@ class ChangeMovementsToChargeable < ActiveRecord::Migration
   def up
     add_column :movements, :chargeable_type, :string
     rename_column :movements, :account_id, :chargeable_id
-    execute %q(update movements set chargeable_type = "Account")
+    execute %q(update movements set chargeable_type = 'Account';)
   end
 
   def down
