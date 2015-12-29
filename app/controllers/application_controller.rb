34a4bb07d72f39d725e395b7ca33826f0d4de2b0
Dmitry Rocha <dmitryrck@gmail.com>
2015-12-29 13:18:36 -0300

    Add authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 require 'application_responder'
 
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+
   self.responder = ApplicationResponder
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate
+    return if Rails.env.test? || (ENV['USERNAME'].blank? || ENV['PASSWORD'].blank?)
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+    end
+  end
 end
