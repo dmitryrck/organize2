770987eb2d4b7c5de406cfc880bee7dc8f57dde2
Dmitry Rocha <dmitryrck@gmail.com>
2019-03-25 15:20:34 +1300

    Add screenshots as path to precompile.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += Rails.root.join("app/assets/images/screenshots/*")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/images/screenshots")
