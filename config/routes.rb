2262d5ceb317d6929e25ad2a13eb9f4376e4cc73
Dmitry L Rocha <dmitryrck@gmail.com>
2024-08-10 11:42:00 -0400

    Add blazer for reporting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  authenticate :admin_user, ->(admin_user) { admin_user.present? } do
+    mount Blazer::Engine, at: "blazer"
+  end
+
   ActiveAdmin.routes(self)
 
   root to: "admin/outgos#index"
